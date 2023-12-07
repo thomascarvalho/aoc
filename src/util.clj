9db8f52220071e14a1362a482b8f61e82e507c6e
Thomas Carvalho <carvalho.thomas@gmail.com>
2023-12-07 16:06:18 +0100

    chore: use also env variable for aoc session

--- src/util.clj
+++ src/util.clj
@@ -82,8 +82,9 @@
         (catch Exception e nil))))
 
 (defn get-session []
-  (let [raw (slurp (str (System/getProperty "user.home") "/.adventofcode.session"))]
-    (str/replace raw "\n" "")))
+  (or (System/getenv "AOC_TOKEN")
+      (let [raw (slurp (str (System/getProperty "user.home") "/.adventofcode.session"))]
+        (str/replace raw "\n" ""))))
 
 (defn load-problem
   "Given a DAY and a YEAR, cache the problem definition locally. If `AOC_TOKEN`
@@ -94,7 +95,6 @@
         path      (fs/path (fs/temp-dir) file-name)]
     (when-not (fs/exists? path)
       (let [resp (->> {:headers {"Cookie" (str "session=" (get-session))
-                                 ;; "User-Agent" (System/getenv "AOC_USER_AGENT")
                                  }}
                       (client/get (format "https://adventofcode.com/%s/day/%s" year day)))]
         (when (= 200 (:status resp))
