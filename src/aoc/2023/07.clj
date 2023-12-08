90780dc0f7c914633d0608ee8d29e3347d259338
Thomas Carvalho <carvalho.thomas@gmail.com>
2023-12-08 23:21:42 +0100

    solve: 2023 07 p2

--- src/aoc/2023/07.clj
+++ src/aoc/2023/07.clj
@@ -38,14 +38,16 @@ KK677 28
 KTJJT 220
 QQQJA 483"))
 
-(defn card-rank [c]
-  (case c
-    \A 14
-    \K 13
-    \Q 12
-    \J 11
-    \T 10
-    (parse-long (str c))))
+(defn card-rank
+  ([c] (card-rank 11 c))
+  ([j-value c]
+   (case c
+     \A 14
+     \K 13
+     \Q 12
+     \J j-value
+     \T 10
+     (parse-long (str c)))))
 
 (defn hand-rank [hand]
   (let [freqs (->> hand
@@ -79,64 +81,52 @@ QQQJA 483"))
 ;; Which gives our answer
 {:nextjournal.clerk/visibility {:code   :hide
                                 :result :show}}
-#_(part-1 input)
+(part-1 input)
 
 ;; ## Part 2
 {:nextjournal.clerk/visibility {:code   :show
                                 :result :hide}}
 
-(defn card-rank-p2 [c]
-  (case c
-    \A 14
-    \K 13
-    \Q 12
-    \J 0
-    \T 10
-    (parse-long (str c))))
+(defn freqs-to-vec [freqs]
+  (->> freqs
+       vals
+       (sort >)
+       vec))
 
-(defn hand-rank-p2 [hand]
-  (let [freqs     (frequencies hand)
-        freqs-vec (->> freqs
-                       vals
-                       (sort >)
-                       vec)
-        j-count   0 #_(or (get freqs \J) 0)
-        j (or (get freqs \J) 0)]
-    (apply conj
-           [(let [[a b c d e] freqs-vec]
-              (+ j
-                 (cond 
-                   (= (+ a j-count) 5) 7
-                   (and (= (+ a j-count) 4) (= b 1) ) 6
-                   (and (= (+ a j-count) 3) (= b 2)) 5
-                   (and (= (+ a j-count) 3) (= b 1) (= c 1)) 4
-                   (and (= (+ a j-count) 2) (= b 2) (= c 1)) 3
-                   (and (= (+ a j-count) 2) (= b 1) (= c 1) (= d 1)) 2
-                   (and (= (+ a j-count) 1) (= b 1) (= c 1)) 1
+(defn rank-p2 [hand]
+  (let [freqs-all       (frequencies hand)
+        freqs-no-jokers (freqs-to-vec (dissoc freqs-all \J))
+        labels          (count freqs-no-jokers)
+        j               (or (get freqs-all \J) 0)
+        a               (or (first freqs-no-jokers) 0)
+        m               (+ j a)]
 
-                ;; [3 1 1] 4
-                ;; [2 2 1] 3
-                ;; [2 1 1 1] 2
-                   )))
-            #_(match freqs-vec
-                [5] 7
-                [4 1] 6
-                [3 2] 5
-                [3 1 1] 4
-                [2 2 1] 3
-                [2 1 1 1] 2
-                [1 _ _ _ _] 1
-                :else (throw (Exception. "Bad match case")))]
-           (mapv card-rank-p2 hand))))
+    (cond
+      (= 5 m) 7
+      (= 4 m) 6
+      (and (= 3 m) (= labels 2)) 5
+      (= 3 m) 4
+      (and (= labels 3) (= j 0)) 3
+      (= 2 m) 2
+      :else 1)))
+
+(defn compare-hands [h1 h2]
+  (let [r1 (rank-p2 h1)
+        r2 (rank-p2 h2)]
+    (cond
+      (< r1 r2) -1
+      (> r1 r2) 1
+      :else (compare (mapv (partial card-rank 0) h1) (mapv (partial card-rank 0) h2)))))
+  
 
 (defn part-2
   [data]
   (->>
    data
-   (map (fn [[hand bid]]
+   #_(map (fn [[hand bid]]
           [(hand-rank-p2 hand) hand bid]))
-   (sort-by first)
-   (map-indexed (fn [idx [_ _ bid]] (* (inc idx) bid)))
+   (sort-by first compare-hands)
+   (map-indexed (fn [idx [_ bid]] (* (inc idx) bid)))
    (reduce +))
   ;
   )
@@ -144,7 +134,7 @@ QQQJA 483"))
 ;; Which gives our answer
 {:nextjournal.clerk/visibility {:code   :hide
                                 :result :show}}
-#_(part-2 input)
+(part-2 input)
 
 
 ;; # Tests
@@ -162,13 +152,11 @@ QQQJA 483"))
   (testing "part two - example"
     (is (= 5905 (part-2 input-example))))
 
-  #_(testing "part two"
-      (is (= 1 (part-2 input)))))
+  (testing "part two"
+      (is (= 254837398 (part-2 input)))))
 
 
 {:nextjournal.clerk/visibility {:code   :hide
                                 :result :show}}
 ;; ## Results
-#_(t/render-results (t/run #'test-2023-07))
-
-(part-2 input)
+(t/run #'test-2023-07)
