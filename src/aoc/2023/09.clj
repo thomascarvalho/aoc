9689e4146e8a057ac6d30a5f9720e84d326d8184
Thomas Carvalho <carvalho.thomas@gmail.com>
2023-12-09 09:26:23 +0100

    solve: 09 2023

---
+++ src/aoc/2023/09.clj
@@ -0,0 +1,121 @@
+^{:nextjournal.clerk/visibility :hide-ns}
+(ns aoc.2023.09
+  {:nextjournal.clerk/toc true}
+  (:require [clojure.java.io :as io]
+            [nextjournal.clerk :as clerk]
+            [util :as u]
+            [test-util :as t]
+            [clojure.string :as str]
+            [clojure.test :refer :all]))
+
+;; # Problem
+{:nextjournal.clerk/visibility {:code   :hide
+                                :result :show}}
+(clerk/html (u/load-problem "09" "2023"))
+{:nextjournal.clerk/visibility {:code   :show
+                                :result :show}}
+
+;; # Solution
+;;
+;; First things first, let's load our input and parse it
+
+(defn parser [data]
+  (->> data
+       str/split-lines
+       (map u/parse-out-longs)))
+
+(def input (->> (slurp (io/resource "inputs/2023/09.txt")) ;; Load the resource
+                parser))                             ;; Split into lines
+{:nextjournal.clerk/visibility {:result :hide}}
+
+;;  Example
+(def input-example (parser "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"))
+
+;; ## Part 1
+(defn part-1
+  [data]
+
+  ;; Quick and dirty solution, but it works, TODO: refactor
+  (->>
+   (for [nums data
+         :let [history (loop [nums nums
+                              all  [nums]]
+                         (if (every? zero? nums)
+                           (reverse all)
+                           (let [res (for [[n1 n2] (partition 2 1 nums)]
+                                       (- n2 n1))]
+                             (recur
+                              res (conj all res)))))]]
+     (->>
+      (map-indexed (fn [i n] [i n]) history)
+      (reduce (fn [h [i n]]
+                (conj h 
+                      (if (zero? i)
+                        (conj n 0)
+                        (concat n [(+ (last n) (last (nth h (dec i))))])))) [])))
+
+   (map #(-> % last last))
+   (reduce +)))
+
+;; Which gives our answer
+{:nextjournal.clerk/visibility {:code   :hide
+                                :result :show}}
+#_(part-1 input)
+
+;; ## Part 2
+{:nextjournal.clerk/visibility {:code   :show
+                                :result :hide}}
+(defn part-2
+  [data]
+  (->>
+   (for [nums data
+         :let [history (loop [nums nums
+                              all  [nums]]
+                         (if (every? zero? nums)
+                           (reverse all)
+                           (let [res (for [[n1 n2] (partition 2 1 nums)]
+                                       (- n2 n1))]
+                             (recur
+                              res (conj all res)))))]]
+     (->>
+      (map-indexed (fn [i n] [i n]) history)
+      (reduce (fn [h [i n]]
+                (conj h
+                      (if (zero? i)
+                        (cons 0 n)
+                        (conj n (- (first n) (first (nth h (dec i)))))))) [])))
+
+   (map #(-> % last first))
+   (reduce +)))
+
+;; Which gives our answer
+{:nextjournal.clerk/visibility {:code   :hide
+                                :result :show}}
+#_(part-2 input)
+
+
+;; # Tests
+{:nextjournal.clerk/visibility {:code   :show
+                                :result :hide}}
+
+;; ## Suite
+(deftest test-2023-09
+
+  (testing "part one - example"
+    (is (= 114 (part-1 input-example))))
+
+  (testing "part one"
+    (is (= 1842168671 (part-1 input))))
+  
+  (testing "part two - example"
+    (is (= 2 (part-2 input-example))))
+
+  (testing "part two"
+      (is (= 903 (part-2 input)))))
+
+{:nextjournal.clerk/visibility {:code   :hide
+                                :result :show}}
+;; ## Results
+#_(t/render-results (t/run #'test-2023-09))
