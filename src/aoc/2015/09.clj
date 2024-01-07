2e80d2770c06dcd4147ed3dc061936aadb708d48
Thomas Carvalho <carvalho.thomas@gmail.com>
2024-01-07 23:27:13 +0100

    solve 9 2015

--- src/aoc/2015/09.clj
+++ src/aoc/2015/09.clj
@@ -4,15 +4,15 @@
   (:require [clojure.java.io :as io]
             [nextjournal.clerk :as clerk]
             [util :as u]
-            [ubergraph.core :as uber]
-            [ubergraph.alg :as alg]
-            [clojure.string :as str]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [clojure.math.combinatorics :as combo]))
 
 ;; # Problem
-;; {:nextjournal.clerk/visibility {:code :hide :result :show}}
-;; (clerk/html (u/load-problem "09" "2015"))
-;; {:nextjournal.clerk/visibility {:code :show :result :show}}
+{:nextjournal.clerk/visibility {:code   :hide
+                                :result :show}}
+(clerk/html (u/load-problem "09" "2015"))
+{:nextjournal.clerk/visibility {:code   :show
+                                :result :show}}
 
 ;; # Solution
 ;;
@@ -22,7 +22,8 @@
   (->> data
        u/to-lines
        (map #(let [[_ from to dist] (re-find #"(\w+) to (\w+) = (\d+)" %)]
-               [from to (parse-long dist)]))))
+               (hash-map (sort [from to]) (parse-long dist))))
+       (apply merge)))
 
 (def input (->> (slurp (io/resource "inputs/2015/09.txt")) ;; Load the resource
                 parser))                             ;; Split into lines
@@ -33,57 +34,44 @@
 London to Belfast = 518
 Dublin to Belfast = 141"))
 
-
-
-(defn hamiltonian-paths [graph start-node]
-  (letfn [(dfs [current-node visited]
-            (if (= (count visited) (uber/count-nodes graph))
-              [visited]
-              (for [next-node (uber/successors graph current-node)
-                    :when     (not (contains? (set visited) next-node))]
-                (dfs next-node (conj visited next-node)))))]
-    (dfs start-node [start-node])))
-
 ;; ## Part 1
 (defn part-1
   [edges]
-  (let [g     (-> (uber/graph)
-                  (uber/add-edges* edges))]
-    (println edges)
+  (let [locations (->> edges
+                       keys
+                       (apply (comp set concat)))]
     (->>
-     (mapcat #(hamiltonian-paths g %) (uber/nodes g))
-     (apply concat)
-     (apply concat)
-     (map #(vector %
-                   (->> (partition 2 1 %)
-                        (mapv (fn [d] (->>
-                                       edges
-                                       (filter (fn [[s e]] (or (= [s e] d) (= [e s] d))))
-                                       (map (fn [[_ _ c]] c)))))
-                        (mapcat concat)
-                        (reduce +))))
-     (map (fn [[_ dist]]
-            dist))
-     (reduce min))))
+     (combo/permutations locations)
+     #_(take 1)
+     (reduce (fn [t locs]
+               (let [dist (reduce (fn [d edge]
+                                    (+ d (get edges (sort edge)))) 0 (partition 2 1 locs))]
+                 (min t dist))) Double/MAX_VALUE))))
 
 ;; Which gives our answer
 {:nextjournal.clerk/visibility {:code   :hide
                                 :result :show}}
-#_(part-1 input)
+(part-1 input)
 
 ;; ## Part 2
 {:nextjournal.clerk/visibility {:code   :show
                                 :result :hide}}
 (defn part-2
-  [input]
-
-  ;
-  )
+  [edges]
+  (let [locations (->> edges
+                       keys
+                       (apply (comp set concat)))]
+    (->>
+     (combo/permutations locations)
+     (reduce (fn [t locs]
+               (let [dist (reduce (fn [d edge]
+                                    (+ d (get edges (sort edge)))) 0 (partition 2 1 locs))]
+                 (max t dist))) 0))))
 
 ;; Which gives our answer
 {:nextjournal.clerk/visibility {:code   :hide
                                 :result :show}}
-#_(part-2 input)
+(part-2 input)
 
 
 ;; # Tests
@@ -92,14 +80,12 @@ Dublin to Belfast = 141"))
 
 ;; ## Suite
 (deftest test-2015-09
-  #_(testing "part one"
-      (is (= 1 (part-1 input))))
+  (testing "part one"
+    (is (= 117 (part-1 input))))
 
-  #_(testing "part two"
-      (is (= 1 (part-2 input)))))
+  (testing "part two"
+    (is (= 909 (part-2 input)))))
 
 {:nextjournal.clerk/visibility {:code   :hide
                                 :result :show}}
 ;; ## Results
-
-(part-1 input)
