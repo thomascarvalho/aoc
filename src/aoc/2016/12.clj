0800602d1fcae009c5925bb9b84cc3596c1f9479
Thomas Carvalho <carvalho.thomas@gmail.com>
2024-04-13 09:11:44 +0200

    solve 12 2016

--- src/aoc/2016/12.clj
+++ src/aoc/2016/12.clj
@@ -3,7 +3,7 @@
   {:nextjournal.clerk/toc true}
   (:require [clojure.java.io :as io]
             [nextjournal.clerk :as clerk]
-            [util :as u] 
+            [util :as u]
             [clojure.string :as str]
             [clojure.test :refer :all]))
 
@@ -16,55 +16,84 @@
 ;;
 ;; First things first, let's load our input and parse it
 
+(defn ->keyword-or-number [c]
+  (when c
+    (if (re-matches #"[a-z]" c)
+      (keyword c)
+      (parse-long c))))
+
+
 (defn parser [data]
   (->> data
-       u/to-lines))
+       u/to-lines
+       (map #(let [[c x y] (str/split % #" ")]
+               [(keyword c) (->keyword-or-number x) (->keyword-or-number y)]))))
 
 (def input (->> (slurp (io/resource "inputs/2016/12.txt")) ;; Load the resource
                 parser))                             ;; Split into lines
-{:nextjournal.clerk/visibility {:result :hide}}
 
 ;;  Example
-(def input-example (parser ""))
+(def input-example (parser "cpy 41 a
+inc a
+inc a
+dec a
+jnz a 2
+dec a"))
+
+(defn process-commands [commands initial-values]
+  (let [max-idx (dec (count commands))]
+
+
+    (->
+     (loop [idx    0
+            values initial-values]
+       (if (> idx max-idx)
+         values
+         (let [[c t1 t2] (nth commands idx)]
+           (case c
+             :cpy (recur
+                   (inc idx)
+                   (if (keyword? t1)
+                     (assoc values t2 (get values t1 0))
+                     (assoc values t2 t1)))
+
+             :inc (recur
+                   (inc idx)
+                   (update values t1 inc))
+
+             :dec (recur
+                   (inc idx)
+                   (update values t1 dec))
+
+             :jnz (if (zero? (get values t1 t1))
+                    (recur
+                     (inc idx)
+                     values)
+                    (recur
+                     (+ idx t2)
+                     values))))))
+     :a)))
 
 ;; ## Part 1
 (defn part-1
-  [data]
-  data
-  ;
-  )
-
-;; Which gives our answer
-{:nextjournal.clerk/visibility {:code :hide :result :show}}
-#_(part-1 input)
+  [commands]
+  (process-commands commands {:a 0
+                              :b 0
+                              :c 0
+                              :d 0}))
 
 ;; ## Part 2
-{:nextjournal.clerk/visibility {:code :show :result :hide}}
 (defn part-2
-  [input]
-  
-  ;
-  )
-
-;; Which gives our answer
-{:nextjournal.clerk/visibility {:code :hide :result :show}}
-#_(part-2 input)
-
-
-;; # Tests
-{:nextjournal.clerk/visibility {:code   :show
-                                :result :hide}}
+  [commands]
+  (process-commands commands {:a 0
+                              :b 0
+                              :c 1
+                              :d 0}))
 
 ;; ## Suite
 (deftest test-2016-12
-  #_(testing "part one"
-    (is (= 1 (part-1 input))))
+  (testing "part one"
+    (is (= 318003 (part-1 input))))
 
-  #_(testing "part two"
-    (is (= 1 (part-2 input)))))
-
-{:nextjournal.clerk/visibility {:code   :hide
-                                :result :show}}
-;; ## Results
-
-(part-1 input-example)
+  (testing "part two"
+    (is (= 9227657 (part-2 input)))))
