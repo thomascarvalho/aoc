cf345f231b17ac8377d720653227d11094c1d033
Thomas Carvalho <carvalho.thomas@gmail.com>
2024-04-20 19:48:57 +0200

    solve 13 2017

--- src/aoc/2017/13.clj
+++ src/aoc/2017/13.clj
@@ -4,7 +4,7 @@
   (:require [clojure.java.io :as io]
             [nextjournal.clerk :as clerk]
             [test-util :as t]
-            [util :as u] 
+            [util :as u]
             [clojure.string :as str]
             [clojure.test :refer :all]))
 
@@ -19,37 +19,103 @@
 
 (defn parser [data]
   (->> data
-       u/to-lines))
+       u/to-lines
+       (map u/parse-out-longs)))
 
 (def input (->> (slurp (io/resource "inputs/2017/13.txt")) ;; Load the resource
                 parser))                             ;; Split into lines
 {:nextjournal.clerk/visibility {:result :hide}}
 
 ;;  Example
-(def input-example (parser ""))
+(def input-example (parser "0: 3
+1: 2
+4: 4
+6: 4"))
 
 ;; ## Part 1
-(defn part-1
-  [data]
-  data)
+
+(defn assoc-initial-layer [layers [x size]]
+  (assoc layers x {:y    0
+                   :op   inc
+                   :size size}))
+
+(defn toggle-op [op]
+  (if (= op inc)
+    dec
+    inc))
+
+(defn move-layers [layers]
+  (->> layers
+       (reduce (fn [layers [x {:keys [y op size]
+                               :as   layer}]]
+                ;; (prn x)
+                ;; (prn layer)
+                 (->>
+                  (let [new-y (op y)]
+                    (if (or (< new-y 0) (>= new-y size))
+                      (let [new-op (toggle-op op)]
+                        (assoc layer :op new-op :y (new-op y)))
+                      (assoc layer :y new-y)))
+                  (assoc layers x))) {})))
+
+(defn part-1 [data]
+  (let [layers (reduce assoc-initial-layer {} data)]
+    (->> (range 0 (inc (apply max (keys layers))))
+         (reduce (fn [{:keys [layers caught]} x]
+                   (prn x)
+                   (let [caught? (try
+                                   (zero? (get-in layers [x :y]))
+                                   (catch Exception e
+                                     false))]
+
+
+                     {:layers (move-layers layers)
+                      :caught (cond-> caught
+                                caught? (conj [x (get-in layers [x :size])]))}))
+                 {:layers layers
+                  :caught []})
+         :caught
+         (map (partial apply *))
+         (apply +))))
+
+
 
 ;; ## Part 2
-{:nextjournal.clerk/visibility {:code :show :result :hide}}
+{:nextjournal.clerk/visibility {:code   :show
+                                :result :hide}}
+
 (defn part-2
   [data]
-  data)
+  (time
+   (let [initial-layers (reduce assoc-initial-layer {} data)
+         layers-range   (range 0 (inc (apply max (keys initial-layers))))]
+     (loop [delay  0
+            layers initial-layers]
+       (let [caught? (->> layers-range
+                          (reduce (fn [layers x]
+                                    (let [caught? (try
+                                                    (zero? (get-in layers [x :y]))
+                                                    (catch Exception e
+                                                      false))]
+                                      (if caught?
+                                        (reduced false)
+                                        (move-layers layers))))
+                                  layers))]
+         (if (false? caught?)
+           (recur (inc delay) (move-layers layers))
+           delay))))))
 
 ;; # Tests
 {:nextjournal.clerk/visibility {:code   :show
                                 :result :hide}}
 (deftest test-2017-13
-  #_(testing "part one"
-    (is (= 1 (part-1 input))))
+  (testing "part one"
+    (is (= 632 (part-1 input))))
 
-  #_(testing "part two"
-    (is (= 1 (part-2 input)))))
+  #_(testing "part two" ;; TODO: optimize, commented because very long
+      (is (= 3849742 (part-2 input)))))
 
 {:nextjournal.clerk/visibility {:code   :hide
                                 :result :show}}
 
-#_(t/test-render #'test-2017-13)
+(t/test-render #'test-2017-13)
