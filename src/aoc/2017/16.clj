09e750c0ffbde364cddff1140c47e0664b3acefe
Thomas Carvalho <carvalho.thomas@gmail.com>
2024-04-27 16:32:51 +0200

    solve 16 2017

--- src/aoc/2017/16.clj
+++ src/aoc/2017/16.clj
@@ -4,52 +4,104 @@
   (:require [clojure.java.io :as io]
             [nextjournal.clerk :as clerk]
             [test-util :as t]
-            [util :as u] 
+            [util :as u]
             [clojure.string :as str]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [clojure.pprint :as pprint]))
 
 ;; # Problem
-;; {:nextjournal.clerk/visibility {:code :hide :result :show}}
-;; (clerk/html (u/load-problem "16" "2017"))
-;; {:nextjournal.clerk/visibility {:code :show :result :show}}
+{:nextjournal.clerk/visibility {:code   :hide
+                                :result :show}}
+(clerk/html (u/load-problem "16" "2017"))
+{:nextjournal.clerk/visibility {:code   :show
+                                :result :show}}
 
 ;; # Solution
 ;;
 ;; First things first, let's load our input and parse it
 
 (defn parser [data]
-  (->> data
-       u/to-lines))
+  (for [cmd  (str/split (str/replace data #"\n" "") #",")
+        :let [op (keyword (str (first cmd)))]]
+    [op
+     (case op
+       :s (u/parse-out-longs cmd)
+       :x (u/parse-out-longs cmd)
+       :p (str/split (subs cmd 1) #"/"))]))
 
 (def input (->> (slurp (io/resource "inputs/2017/16.txt")) ;; Load the resource
                 parser))                             ;; Split into lines
 {:nextjournal.clerk/visibility {:result :hide}}
 
 ;;  Example
-(def input-example (parser ""))
+(def input-example (parser "s1,x3/4,pe/b"))
+
+(defn do-the-dance [cmds programs]
+  (->> cmds
+       (reduce (fn [programs [op [a b]]]
+                 (case op
+                   :s (let [spins    (take-last a programs)
+                            programs (drop-last a programs)]
+                        (into [] (concat spins programs)))
+                   :x (let [a-val (nth programs a)
+                            b-val (nth programs b)]
+                        (-> programs
+                            (assoc a b-val)
+                            (assoc b a-val)))
+                   :p (let [a-idx (.indexOf programs a)
+                            b-idx (.indexOf programs b)]
+                        (-> programs
+                            (assoc a-idx b)
+                            (assoc b-idx a))))) programs)))
 
 ;; ## Part 1
 (defn part-1
   [data]
-  data)
+  (let [programs ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p"]
+        cmds     data]
+    (->> (do-the-dance cmds programs)
+         (str/join ""))))
 
 ;; ## Part 2
-{:nextjournal.clerk/visibility {:code :show :result :hide}}
+{:nextjournal.clerk/visibility {:code   :show
+                                :result :hide}}
+
 (defn part-2
   [data]
-  data)
+  (let [programs                   ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p"]
+        cmds                       data
+        dance                      (partial do-the-dance cmds)
+
+        [progs offset-idx n-cycle] (loop [times         0
+                                          programs      programs
+                                          last-programs []]
+                                     (let [program (dance programs)
+                                           idx     (.indexOf last-programs program)]
+                                       (if (or (= times 1000) (pos? idx))
+                                         [last-programs idx times]
+                                         (recur
+                                          (inc times)
+                                          program
+                                          (conj last-programs
+                                                program)))))]
+
+    (->> n-cycle
+         (mod 1000000000)
+         (+ offset-idx)
+         (nth progs)
+         (str/join ""))))
 
 ;; # Tests
 {:nextjournal.clerk/visibility {:code   :show
                                 :result :hide}}
 (deftest test-2017-16
-  #_(testing "part one"
-    (is (= 1 (part-1 input))))
+  (testing "part one"
+    (is (= "bkgcdefiholnpmja" (part-1 input))))
 
-  #_(testing "part two"
-    (is (= 1 (part-2 input)))))
+  (testing "part two"
+    (is (= "knmdfoijcbpghlea" (part-2 input)))))
 
 {:nextjournal.clerk/visibility {:code   :hide
                                 :result :show}}
 
-#_(t/test-render #'test-2017-16)
+(t/test-render #'test-2017-16)
